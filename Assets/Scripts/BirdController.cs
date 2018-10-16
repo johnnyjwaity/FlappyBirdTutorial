605d5eaa141e336333b1140d93d18300742a147d
johnnyjwaity <johnnywaity@gmail.com>
2018-10-15 21:03:40 -0500

    Part 2

---
+++ Assets/Scripts/BirdController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour {
+
+    //Store the Rigidbody2D
+    private Rigidbody2D rb;
+    //Variable for affecting the strength of the bird
+    public float strength;
+
+    
+
+	// Use this for initialization
+	void Start () {
+        //Get The Rigidbody2D from the bird
+        rb = GetComponent<Rigidbody2D>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // This will change the velocity of the bird to go upwards whenever Space is pressed
+            rb.velocity = Vector2.up * strength;
+        }
+	}
+}
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # FlappyBirdTutorial
 
-Flappy Bird Tutorial For Tech CLub
+Flappy Bird Tutorial For Tech Club
 
 [Part One Slideshow](https://docs.google.com/presentation/d/1Y6R6jFd2p1FG4XyLaFrBuNGr5eMjSCL3aFaWioziu3g/edit?usp=sharing)
+
+[Part Two Slideshow](https://docs.google.com/presentation/d/1qjx9FfpdNPWbClj_awCDKMFHd1hMC5165fh4jS4qJRg/edit?usp=sharing)
